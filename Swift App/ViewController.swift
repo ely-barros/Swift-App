60c91b0570d976459d87d0df23da4858245b59c5
ely-barros <elyb@ely-macbook-pro.br.ibm.com>
2018-10-29 07:50:29 -0300

    Adding operations

--- Swift App/ViewController.swift
+++ Swift App/ViewController.swift
@@ -17,8 +17,7 @@ class ViewController: UIViewController {
     @IBOutlet weak var text2: UITextField!
     
     @IBAction func buttonTapped(_ sender: Any) {
-        print(text1.text!)
-        print(text2.text!)
+        myLabel.text = "Answer is \(Double(text1.text!)! + Double(text2.text!)!)"
     }
     
     override func viewDidLoad() {
